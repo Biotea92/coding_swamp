5c4b50d00166045d9469397ded41c18acf2ab19a
Kim woo jong <skekdnwhd@gmail.com>
2023-01-26 12:34:22 +0900

    chore:project init

---
+++ backend/swamp/src/main/java/coding/swamp/SwampApplication.java
@@ -0,0 +1,13 @@
+package coding.swamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwampApplication.class, args);
+	}
+
+}
