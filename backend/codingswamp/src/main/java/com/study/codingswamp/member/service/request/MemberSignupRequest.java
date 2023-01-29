b607bb00e04078172dfa3851d897518a9a3d5095
Kim woo jong <skekdnwhd@gmail.com>
2023-01-30 03:19:26 +0900

    refactor: setter 제거

--- backend/codingswamp/src/main/java/com/study/codingswamp/member/service/request/MemberSignupRequest.java
+++ backend/codingswamp/src/main/java/com/study/codingswamp/member/service/request/MemberSignupRequest.java
@@ -2,7 +2,6 @@ package com.study.codingswamp.member.service.request;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,27 +10,26 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-@Setter
 @Getter
 @ToString
 public class MemberSignupRequest {
 
     @NotBlank
     @Email
-    private String email;
+    private final String email;
 
     @NotBlank
     @Pattern(
             regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@!%*#?&])[A-Za-z\\d$@!%*#?&]{8,}$",
             message = "최소 8자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자를 포함해야합니다."
     )
-    private String password;
+    private final String password;
 
     @NotBlank
     @Length(min = 3, message = "최소 3자 이상이어야 합니다.")
-    private String username;
+    private final String username;
 
-    private MultipartFile imageFile;
+    private final MultipartFile imageFile;
 
     @Builder
     public MemberSignupRequest(String email, String password, String username, MultipartFile imageFile) {
